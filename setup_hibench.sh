983a7596f120406a0de5e27dc8597e5fb4f30649
Efthymis Papageorgiou <papageorgiou.efthymis@protonmail.com>
2024-05-07 17:44:04 +0300

    fix: pattern matching

--- setup_hibench.sh
+++ setup_hibench.sh
@@ -12,16 +12,14 @@ mvn -Dspark=3.1 -Dscala=2.12 clean package
 cp conf/hadoop.conf.template conf/hadoop.conf
 hadoop_home="/usr/lib/hadoop"
 hdfs_master=$(hdfs getconf -confKey fs.defaultFS | grep -o 'hdfs://[^"]*')
-sed -i "s|hibench.hadoop.home.*|hibench.hadoop.home $hadoop_home|" conf/hadoop.conf
+sed -i "/^hibench.hadoop.home /s|hibench.hadoop.home.*|hibench.hadoop.home $hadoop_home|" conf/hadoop.conf
 sed -i "s|hibench.hdfs.master.*|hibench.hdfs.master $hdfs_master|" conf/hadoop.conf
-
 echo "hibench.hadoop.examples.jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-examples.jar" >> conf/hadoop.conf
 echo "hibench.hadoop.examples.test.jar /usr/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient.jar" >> conf/hadoop.conf
 
 #Configure Spark
 cp conf/spark.conf.template conf/spark.conf
 spark_home="/usr/lib/spark"
-sed -i "s|hibench.spark.home.*|hibench.spark.home $spark_home|" conf/spark.conf
+sed -i "/^hibench.spark.home /s|hibench.spark.home.*|hibench.spark.home $spark_home|" conf/spark.conf
 sed -i "s|hibench.spark.master.*|hibench.spark.master yarn|" conf/spark.conf
-
 echo "Setup completed successfully."
